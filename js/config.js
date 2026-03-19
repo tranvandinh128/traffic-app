e52c9ea1a3aa1b55bd772a0021c3fbf1cdb5054d
Trần Văn Dĩnh <tranvandinh1281984@gmail.com>
2026-03-19 19:30:50 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,6 @@
-
+const examConfig = {
+  A1: { total: 25, pass: 21, time: 19 },
+  A2: { total: 25, pass: 23, time: 19 },
+  B1: { total: 35, pass: 32, time: 22 },
+  B2: { total: 35, pass: 32, time: 22 }
+};
