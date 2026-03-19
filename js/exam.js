157c2496eaeb65a4b0da490728f0d33340a83212
Trần Văn Dĩnh <tranvandinh1281984@gmail.com>
2026-03-19 19:32:44 +0700

    Update exam.js

--- js/exam.js
+++ js/exam.js
@@ -1,1 +1,28 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+<meta charset="UTF-8">
+<title>Thi</title>
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
 
+<div class="container">
+  <div class="timer">⏱ <span id="time"></span></div>
+
+  <h3 id="index"></h3>
+  <h2 id="question"></h2>
+
+  <div id="answers"></div>
+
+  <button onclick="next()">Tiếp</button>
+  <button onclick="submitExam()">Nộp bài</button>
+</div>
+
+<script src="js/questions.js"></script>
+<script src="js/config.js"></script>
+<script src="js/utils.js"></script>
+<script src="js/exam.js"></script>
+
+</body>
+</html>
