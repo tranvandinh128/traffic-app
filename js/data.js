277157c1f578f4f55b61f35eb571112e6211ad60
Trần Văn Dĩnh <tranvandinh1281984@gmail.com>
2026-03-19 19:02:34 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,1 +1,16 @@
-
+const questions = [
+  {
+    id: 1,
+    question: "Đèn đỏ có được đi không?",
+    answers: ["Được", "Không", "Tùy", "Có thể"],
+    correct: 1,
+    is_critical: true
+  },
+  {
+    id: 2,
+    question: "Đi bên nào là đúng?",
+    answers: ["Bên trái", "Bên phải", "Ở giữa", "Tùy"],
+    correct: 1,
+    is_critical: false
+  }
+];
