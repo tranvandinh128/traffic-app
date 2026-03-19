64d83dfeaf3b6cc4415028f6f026a3d26a8738ab
Trần Văn Dĩnh <tranvandinh1281984@gmail.com>
2026-03-19 19:36:24 +0700

    Update review.js

--- js/review.js
+++ js/review.js
@@ -1,1 +1,25 @@
+let license = localStorage.getItem("license");
+let questions = getByLicense(license);
 
+let current = 0;
+
+function load(){
+  let q = questions[current];
+
+  document.getElementById("question").innerText = q.question;
+
+  let html = "";
+  q.answers.forEach((a,i)=>{
+    let color = i === q.correct ? "green" : "black";
+    html += `<button style="color:${color}">${a}</button>`;
+  });
+
+  document.getElementById("answers").innerHTML = html;
+}
+
+function next(){
+  current = (current + 1) % questions.length;
+  load();
+}
+
+load();
