e239f20a23d058aaef8f26575652cd9cc27cf8ac
Trần Văn Dĩnh <tranvandinh1281984@gmail.com>
2026-03-19 19:31:45 +0700

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -1,1 +1,20 @@
-
+const allQuestions = [
+  {
+    id: 1,
+    question: "Đèn đỏ có được đi không?",
+    answers: ["Được", "Không", "Tùy", "Có thể"],
+    correct: 1,
+    category: "law",
+    license: ["A1","A2","B1","B2"],
+    is_critical: true
+  },
+  {
+    id: 2,
+    question: "Đi bên nào đúng?",
+    answers: ["Trái","Phải","Giữa","Tùy"],
+    correct: 1,
+    category: "law",
+    license: ["A1","A2"],
+    is_critical: false
+  }
+];
