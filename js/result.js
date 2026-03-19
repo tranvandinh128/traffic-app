50fc50755b29773637451c153bd3a76613381da8
Trần Văn Dĩnh <tranvandinh1281984@gmail.com>
2026-03-19 19:35:31 +0700

    Update result.js

--- js/result.js
+++ js/result.js
@@ -1,2 +1,30 @@
-let score = localStorage.getItem("score");
-document.getElementById("score").innerText = "Bạn đúng: " + score;
+let answers = JSON.parse(localStorage.getItem("answers"));
+let questions = JSON.parse(localStorage.getItem("questions"));
+let license = localStorage.getItem("license");
+
+let config = examConfig[license];
+
+let score = 0;
+let failedCritical = false;
+
+questions.forEach((q,i)=>{
+  if(answers[i] == q.correct){
+    score++;
+  } else if(q.is_critical){
+    failedCritical = true;
+  }
+});
+
+let text = `Hạng ${license}\n`;
+text += `Điểm: ${score}/${config.total}\n`;
+
+if(failedCritical){
+  text += "❌ Sai câu liệt → TRƯỢT";
+}
+else if(score >= config.pass){
+  text += "✅ ĐẠT";
+}else{
+  text += "❌ KHÔNG ĐẠT";
+}
+
+document.getElementById("result").innerText = text;
